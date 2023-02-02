5db08309b4f091329ebbb759f6be7e312aae1833
Lokocredo <macedot2004@gmail.com>
2023-02-02 14:10:18 -0300

    Inserindo arquivo principal.js

---
+++ principal.js
@@ -0,0 +1,9 @@
+var banners = ["Os melhores do Brasil!", "Qualidade e preço baixo!"];
+var bannerAtual = 0;
+
+function trocaBanner() {
+	bannerAtuak = (bannerAtual + 1) % 2;
+	document.querySelector('h2#mensagem').textContent = banners[bannerAtual];
+}
+
+setInterval(trocaBanner, 2000);
